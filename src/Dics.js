bf7ab65e0af5f3855f60dcb6d6319b4d1b66c574
henny <kimhyein1105@gmail.com>
2023-12-28 21:20:35 +0900

    컴포넌트 이미지 불러오기 이해

---
+++ src/Dics.js
@@ -0,0 +1,8 @@
+import diceBlue01 from './assets/dice-blue-1.svg';
+// 이미지를 import로 불러오기
+
+function Dice() {
+	return <img src={diceBlue01} alt='주사위' />;
+}
+
+export default Dice;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,4 @@
 import ReactDOM from 'react-dom';
+import App from './App';
 
-// const element = <h1>안녕 리액트!</h1>;
-function Hello() {
-	// 꼭 첫글자는 대문자로 만들기
-	return <h1>안녕 리액트</h1>;
-}
-// 리액트 컴포넌트
-// 리액트 엘리먼트를 리액트 컴포넌트로 만들어서 활용
-//
-
-const element = (
-	<>
-		<Hello />
-		<Hello />
-		<Hello />
-	</>
-);
-
-console.log(element);
-// 리액트 엘리먼트
-// ReactDOM에 render 메소드로 전달하게 되면 리액트가 이 객체를 해석해서 HTML로 해석하게 됨
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
