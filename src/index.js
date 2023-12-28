9e94a07e2974713a4f0062bffc9d9d7413620906
henny <kimhyein1105@gmail.com>
2023-12-28 21:15:03 +0900

    컴포넌트의 이해

--- src/index.js
+++ src/index.js
@@ -1,26 +1,23 @@
 import ReactDOM from 'react-dom';
 
-const product = 'MacBook';
-const model = ' Air';
-const item = product + model;
-const imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/MacBook_LMSD_Issue_2009.jpeg/220px-MacBook_LMSD_Issue_2009.jpeg';
-
-function handleClick() {
-	alert('곧 도착합니다.');
+// const element = <h1>안녕 리액트!</h1>;
+function Hello() {
+	// 꼭 첫글자는 대문자로 만들기
+	return <h1>안녕 리액트</h1>;
 }
+// 리액트 컴포넌트
+// 리액트 엘리먼트를 리액트 컴포넌트로 만들어서 활용
+//
 
-// 자바스크립트 문법을 사용하려면 중괄호로 써주기
-// 변수로 사용할 수 있고 자바스크립트로 된 표현식은 모두 사용 가능
-ReactDOM.render(
+const element = (
 	<>
-		<h1>나만의 {product.toUpperCase()} 주문하기</h1>
-		<h1>나만의 {item} 주문하기</h1>
-		<img src={imageUrl} alt='제품 사진' />
-		<button onClick={handleClick}>확인</button>
-	</>,
-	// 이미지 태그에도 따옴표가 아니라 {} 중괄호로 감싸기
-	document.getElementById('root')
+		<Hello />
+		<Hello />
+		<Hello />
+	</>
 );
 
-// JSX에서 이벤트 핸들러를 등록할 때 addEventListenser보다는
-// 이 요소의 속성값으로 이벤트 핸들러를 등록
+console.log(element);
+// 리액트 엘리먼트
+// ReactDOM에 render 메소드로 전달하게 되면 리액트가 이 객체를 해석해서 HTML로 해석하게 됨
+ReactDOM.render(element, document.getElementById('root'));
