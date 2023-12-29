012d1dd6de2c3d9ebcde319db986aee897c3ddcb
henny <kimhyein1105@gmail.com>
2023-12-29 21:25:34 +0900

    Destructuring 문법 활용

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Dice from './Dics';
 function App() {
 	return (
 		<div>
-			<Dice color='red' />
+			<Dice color='red' num={2} />
 		</div>
 	);
 }
-
+// 자바스크립트에서 숫자를 쓰려면 반드시 중괄호 활용하기
 export default App;
