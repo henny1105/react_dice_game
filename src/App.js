ac0a462586c49cd6f34db1c5dec8f92278e50db3
henny <kimhyein1105@gmail.com>
2023-12-31 16:32:23 +0900

    참조형 state

--- src/App.js
+++ src/App.js
@@ -2,32 +2,52 @@ import { useState } from 'react';
 import Button from './Button';
 import Dice from './Dics';
 
-// 랜덤 함수
 function random(n) {
 	return Math.ceil(Math.random() * n);
 }
 
 function App() {
 	const [num, setNum] = useState(1);
+	const [sum, setSum] = useState(0);
+	const [gameHistory, setgameHistory] = useState([]);
 
 	const handleRollClick = () => {
 		const nextNum = random(6);
 		setNum(nextNum);
+		setSum(sum + nextNum);
+
+		// gameHistory 가 배열이기 때문에 기본형이 아니라 참조형임
+		// 기록들을 가진 배열 자체를 값으로 갖는게 아니라
+		// 배열을 가리키고 있는 주솟값을 가지고 있는 것임
+
+		// gameHistory.push(nextNum);
+		// setgameHistory(gameHistory);
+		setgameHistory([...gameHistory, nextNum]);
+		// spread 문법 활용
 	};
-	// useState 함수는 파라미터로 초기값을 전달받음
-	// 이 함수가 실행된 다음에는 배열의 형태로 요소 두개를 리턴하게 됨
-	// 첫번째는 변수의 상태, 두번째는 setter 함수 -> 함수를 호출할 때 파라미터로 전달하는 값으로 state 값이 변경됨
 
 	const handleClearClick = () => {
 		setNum(1);
+		setSum(0);
+		setgameHistory([]);
 	};
+
 	return (
 		<div>
 			<div>
 				<Button onClick={handleRollClick}>던지기</Button>
 				<Button onClick={handleClearClick}>처음부터</Button>
 			</div>
-			<Dice color='red' num={num} />
+			<div>
+				<h2>나</h2>
+				<Dice color='blue' num={num} />
+				<h2>총점</h2>
+				<p>{sum}</p>
+				<h2>기록</h2>
+				<p>{gameHistory.join(', ')}</p>
+				{/* 아규먼트로 전달한 이 값을 배열의 각 요소들 사이사이에 넣어서
+				결과적으로는 하나의 문자열로 만들어주는 메소드 */}
+			</div>
 		</div>
 	);
 }
