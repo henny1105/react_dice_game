539ff3e3400271d1a3d42ac84ea49f83b9b311b8
henny <kimhyein1105@gmail.com>
2023-12-31 17:21:31 +0900

    인라인 스타일

--- src/Button.js
+++ src/Button.js
@@ -1,8 +1,32 @@
-function Button({ children, onClick }) {
-	return <button onClick={onClick}>{children}</button>;
+const baseButtonStyle = {
+	padding: '14px 27px',
+	borderRadius: '30px',
+	outline: 'none',
+	cursor: 'pointer',
+	fontSize: '17px',
+};
+
+const blueButtonStyle = {
+	...baseButtonStyle,
+	backgroundColor: 'rgba(0,89,255,0.2)',
+	border: 'solid 1px #7090ff',
+	color: '#7090ff',
+};
+
+const redButtonStyle = {
+	...baseButtonStyle,
+	backgroundColor: 'rgba(255,78,78,0.2)',
+	border: 'solid 1px #ff4664',
+	color: '#ff4664',
+};
+
+function Button({ children, onClick, color }) {
+	const style = color === 'red' ? redButtonStyle : blueButtonStyle;
+	return (
+		<button style={style} onClick={onClick}>
+			{children}
+		</button>
+	);
 }
 
 export default Button;
-
-// 컴포넌트 함수에서 따로 가공하지 않고
-// 단순히 보여주기만 할 모습은 props children을 사용할 수 있음
